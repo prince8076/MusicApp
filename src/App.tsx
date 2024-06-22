6172f6899b740a5dc3869c9119f193a1edb46959
prince8076 <rahulsingh00c@gmail.com>
2024-06-22 23:02:09 +0530

    done

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+      <SafeAreaView>
+        <StatusBar/>
+            <View>
+              <Text>App</Text>
+            </View>
+      </SafeAreaView>
+    
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
